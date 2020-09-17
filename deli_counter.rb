6c8bbfe3279ed36157e92ef6b386dd317eb190b1
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-17 18:40:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,18 @@ def line(array)
      puts "The line is currently empty."
  end
 end
+
+def take_a_number(array, name)
+  i=0 
+  counter = 1
+  while array.count != name.length 
+    array[i] = counter
+    i+=1 
+    counter +=1 
+  end
+  if array.count > 1
+    puts "The line is currently:"
+  end
+  name.each_with_index {|val, index| puts "#{index+1}. #{val}"}
+  end
+  
